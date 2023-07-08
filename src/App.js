df743e821c963c115c03a9efba778f965550468e
Baurzhan Yussupov <baurzhangulnur@gmail.com>
2023-07-08 14:18:26 +0600

    Layout has been created and routes for new components have been prepared

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<h3 style={{ textAlign: "center" }}>Component News will be here</h3>} />
+          <Route path="/news" element={<h3 style={{ textAlign: "center" }}>Component News will be here</h3>} />
+          <Route path="/news/add" element={<h3 style={{ textAlign: "center" }}>Component Add New Post will be here</h3>} />
+          <Route path="/news/:newsId" element={<h3 style={{ textAlign: "center" }}>Component Full Post will be here</h3>} />
+          <Route path="*" element={<h3 style={{ textAlign: "center" }}>Page not found</h3>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter >
   );
 }
 
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Toolbar.css";
+
+const Toolbar = () => (
+    <header className="Toolbar">
+        <div className="Toolbar-logo">
+            <span>
+                News
+            </span>
+        </div>
+    </header>
+);
+
+export default Toolbar;
